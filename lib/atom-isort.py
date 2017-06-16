070e5fc06a040e41c65cbb55aaadc0c1bf166837
Arlo Shallit <arloshallit@Arlos-MacBook-Pro.local>
2017-06-16 18:18:11 -0400

    some docstring and comment changes

--- lib/atom-isort.py
+++ lib/atom-isort.py
@@ -11,6 +11,21 @@ class IsortTools(object):
         self._input = io.open(sys.stdin.fileno(), encoding='utf-8')
 
     def _serialize(self, response_type, response_dict):
+        '''Returns a json dump combining response_dict and response_type.
+
+        response_type is keyed to 'type', and that dict is merged with
+        response_dict (without overwriting response_dict).
+
+        Args:
+            response_type (str): The type of json response, eg:
+                'sort_text', 'check_text', 'error'
+            response_dict (dict): The dictionary of response values, eg:
+                'new_contents':(str), 'correctly_sorted':(bool)
+
+        Returns:
+            A json dump of response_dict, with the added key-value pair of
+            'type':response_type.
+        '''
         retdict = {'type': response_type}
         retdict.update(response_dict)
         return json.dumps(retdict)
@@ -31,12 +46,13 @@ class IsortTools(object):
                                 {'new_contents': new_contents}))
 
         elif request['type'] == 'check_text':
-            # Some explanation required:
+            #### Some explanation required: ####
             # Since we are using stdout, we can't use the default 'check=True'
             # option, since isort will write to sdtout if there are errors,
-            # and this cannot be overridden. However, we can replicate the
-            # behavior by sorting imports and then comparing to the unsorted
-            # text. If they are different, then they are not sorted.
+            # and this cannot be overridden with a function call. However, we
+            # can replicate the behavior by sorting imports and then comparing
+            # to the unsorted text. If they are different, then they are not
+            # sorted.
             if len(request['source'].split()) == 0:
                 correctly_sorted = True
             else:
